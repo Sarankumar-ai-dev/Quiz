5a8b92b0740a83cf8bcbbf251921bbd8dc99e073
saran <sarankumar10307@gmail.com>
2026-04-07 05:14:30 +0530

    files are changed

--- backend/Quiz_backend/settings.py
+++ backend/Quiz_backend/settings.py
@@ -1,20 +1,21 @@
 from pathlib import Path
-
-BASE_DIR = Path(__file__).resolve().parent.parent
-
 import os
 import dj_database_url
 from dotenv import load_dotenv
 
+
+BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-SECRET_KEY = os.getenv('SECRET_KEY')
+# SECURITY
+SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-key')
 
-DEBUG = os.getenv('DEBUG') == 'True'
-
+DEBUG = os.getenv('DEBUG', 'True') == 'True'
 
 ALLOWED_HOSTS = ['*']
 
+
+# INSTALLED APPS
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -27,6 +28,8 @@ INSTALLED_APPS = [
     'quiz',
 ]
 
+
+# MIDDLEWARE
 MIDDLEWARE = [
     'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
@@ -38,8 +41,11 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+
 ROOT_URLCONF = 'Quiz_backend.urls'
 
+
+# TEMPLATES
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
@@ -56,12 +62,20 @@ TEMPLATES = [
     },
 ]
 
+
 WSGI_APPLICATION = 'Quiz_backend.wsgi.application'
 
-DATABASES = {
-    'default': dj_database_url.parse(os.getenv("DATABASE_URL"))
-}
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.parse(DATABASE_URL)
+    }
+
+
+
+# PASSWORD VALIDATION
 AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator'},
     {'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator'},
@@ -69,8 +83,13 @@ AUTH_PASSWORD_VALIDATORS = [
     {'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator'},
 ]
 
+
+# CORS
 CORS_ALLOW_ALL_ORIGINS = True
 
+
+# STATIC FILES
 STATIC_URL = 'static/'
 
+# DEFAULT PRIMARY KEY
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
