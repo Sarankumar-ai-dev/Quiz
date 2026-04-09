aa94099a7526cff30e03cfd87535f94d35b724f7
saran <sarankumar10307@gmail.com>
2026-04-10 05:18:22 +0530

    render deploy

--- backend/Quiz_backend/settings.py
+++ backend/Quiz_backend/settings.py
@@ -7,7 +7,7 @@ from dotenv import load_dotenv
 BASE_DIR = Path(__file__).resolve().parent.parent
 load_dotenv()
 
-# SECURITY
+
 SECRET_KEY = os.getenv('SECRET_KEY', 'django-insecure-key')
 
 DEBUG = os.getenv('DEBUG', 'True') == 'True'
